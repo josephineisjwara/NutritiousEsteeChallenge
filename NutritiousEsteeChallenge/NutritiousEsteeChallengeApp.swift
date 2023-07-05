b7c240312c37148546000855386aa178bd769572
Josephine Aurelia Isjwara <josephine.isjwara@gmail.com>
2023-07-05 15:27:36 +0700

    Initial Commit

---
+++ NutritiousEsteeChallenge/NutritiousEsteeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutritiousEsteeChallengeApp.swift
+//  NutritiousEsteeChallenge
+//
+//  Created by Josephine Aurelia Isjwara on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct NutritiousEsteeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
